0d6b7cf200208c9db026bc416d6ac155db68cb94
Sergey Bogdanov <cj.bogdanov@gmail.com>
2017-06-04 12:32:21 +0300

    Add data for slider

---
+++ markup/components/slider/data/data.js
@@ -0,0 +1,10 @@
+data = {
+  slider: {
+    defaults: {
+      values: [
+        {src: 'slider-01.jpg', alt: 'Slider 1'},
+        {src: 'slider-02.jpg', alt: 'Slider 2'},
+      ]
+    }
+  }
+};
--- markup/components/slider/slider.js
+++ markup/components/slider/slider.js
@@ -1,12 +1,15 @@
-$( document ).ready( function () {
-    let slider = $( '.slider__items' );
+$(document).ready(function() {
+  let slider = $('.slider__items');
 
-    slider.css( 'display', 'block' ); // Для предотвращения flash эффекта
+  slider.css('display', 'block'); // Для предотвращения flash эффекта
 
-    slider.slick({
-        //  autoplay: true,
-        arrows: true,
-        appendArrows: '.slider__arrows',
-        zIndex: 5
-    });
+  slider.slick({
+    autoplay: true,
+    arrows: true,
+    appendArrows: '.slider__arrows',
+    infinite: true,
+    speed: 1500,
+    fade: true,
+    zIndex: 5
+  });
 });
