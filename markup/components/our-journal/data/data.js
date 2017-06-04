9f47e4ef1c43f12275f8a9ce6b7f34385540df92
Sergey Bogdanov <cj.bogdanov@gmail.com>
2017-06-04 12:49:15 +0300

    Add data for our-journal

---
+++ markup/components/our-journal/data/data.js
@@ -0,0 +1,11 @@
+data = {
+  ourJournal: {
+    defaults: {
+      values: [
+        {src: 'our-journal-1.jpg', alt: 'Our Journal 1'},
+        {src: 'our-journal-2.jpg', alt: 'Our Journal 2'},
+        {src: 'our-journal-3.jpg', alt: 'Our Journal 3'},
+      ]
+    }
+  }
+};
