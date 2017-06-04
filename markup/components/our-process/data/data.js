8e0a9e5fc8d0bdef76180b5f3ea9181e1cbcf062
Sergey Bogdanov <cj.bogdanov@gmail.com>
2017-06-04 12:55:17 +0300

    Add data for our-process

---
+++ markup/components/our-process/data/data.js
@@ -0,0 +1,12 @@
+data = {
+  ourProcess: {
+    defaults: {
+      values: [
+        {iconName: 'our-process-bulbs', className: 'our-process__bulbs'},
+        {iconName: 'our-process-rocket', className: 'our-process__rocket'},
+        {iconName: 'our-process-chemistry', className: 'our-process__chemistry'},
+        {iconName: 'our-process-award', className: 'our-process__award'}
+      ]
+    }
+  }
+};
