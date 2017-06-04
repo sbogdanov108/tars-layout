4b7f37f36c6ca1fe4381e91eaa48a241c38c8134
Sergey Bogdanov <cj.bogdanov@gmail.com>
2017-06-04 13:03:56 +0300

    Add data for our-projects

---
+++ markup/components/our-projects/data/data.js
@@ -0,0 +1,11 @@
+data = {
+  ourProjects: {
+    defaults: {
+      values: [
+        {src: 'recent-project-1.jpg', alt: 'Our Project 1', title: 'Disk cover Mockup'},
+        {src: 'recent-project-2.jpg', alt: 'Our Project 2', title: 'John Doe business Card'},
+        {src: 'recent-project-3.jpg', alt: 'Our Project 3', title: 'Infographic Elements'}
+      ]
+    }
+  }
+};
