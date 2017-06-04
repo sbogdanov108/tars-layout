36635f42efd365f6ef7c2c6f2b34c7c670499b38
Sergey Bogdanov <cj.bogdanov@gmail.com>
2017-06-04 14:15:09 +0300

    Fixed header menu animation

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -1,7 +1,7 @@
 $('.header__has-submenu').hover(function() {
   let dropdownMenu = $(this).find('.header__submenu').eq(0);
 
-  dropdownMenu.delay(200).fadeIn(500);
+  dropdownMenu.stop(true, true).delay(200).fadeIn(500);
 }, function() {
-  $(this).find('.header__submenu').eq(0).delay(200).fadeOut(500);
+  $(this).find('.header__submenu').eq(0).stop(true, true).delay(200).fadeOut(500);
 });
