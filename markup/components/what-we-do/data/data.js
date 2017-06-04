7d1d3e1574c0c368248b734f9825587a90e0486d
Sergey Bogdanov <cj.bogdanov@gmail.com>
2017-06-04 13:10:09 +0300

    Add data for what-we-do

---
+++ markup/components/what-we-do/data/data.js
@@ -0,0 +1,12 @@
+data = {
+  whatWeDo: {
+    defaults: {
+      values: [
+        {src: 'what-we-do-1.png', alt: 'What we do 1'},
+        {src: 'what-we-do-2.png', alt: 'What we do 2'},
+        {src: 'what-we-do-3.png', alt: 'What we do 3'},
+        {src: 'what-we-do-4.png', alt: 'What we do 4'}
+      ]
+    }
+  }
+};
